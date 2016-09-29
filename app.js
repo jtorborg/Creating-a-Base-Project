5afc18ff9f30d6ded8d71d843a1b7f02157c9757
Jacquelyn <jtorborg1227@comcast.net>
2016-09-28 20:45:31 -0500

    added jquery functionality

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("This file was created on September 20, 2016 by Jackie Torborg.");
+
+
+//add jquery doc.ready function and event listeners
+
+console.log("app.js is working");
+
+$(document).ready(function() {
+
+console.log("jQuery is working");
+
+});
